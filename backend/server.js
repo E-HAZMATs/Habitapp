3805dfa5b1e4d7b3998763e79a0c1caf10840908
E-HAZMATs <alzamarkal@gmail.com>
2025-11-20 22:56:07 +0300

    Setup my express instance. Connected express to the DB.

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require ('express');
+const cors = require('cors');
+require('dotenv').config();
+const {sequelize} = require('./models')
+
+async function connectToDB(){
+    try{
+        await sequelize.authenticate();
+        console.log('Connected to DB ^_^')
+    }
+    catch (error) {
+        console.error("Something went wrong while connecting to the DB!", error)
+    }
+}
+
+async function startServer() {
+
+    const app = express();
+    
+    app.use(cors({
+        origin: 'http://localhost:4000' // My angular URL. Didn't set it up yet.
+    }));
+    app.use(express.json())
+    
+    app.get('/', (req, res) => {
+        res.send('server running...')
+    })
+    
+    const PORT = process.env.PORT || 3333;
+    
+    await connectToDB();
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+}
+
+startServer();
+
+
+
