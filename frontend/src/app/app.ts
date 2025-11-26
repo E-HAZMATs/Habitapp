1b36d4dd60dd3c8361fc7912825d701d12e4e16b
E-HAZMATs <alzamarkal@gmail.com>
2025-11-26 12:26:52 +0300

    Setup ngx-translate. May, may cause some issues, for some reason i had to move the assets folder (auto generated by ng) to /src instead of /src/app. ngx-translate couldn't access it before for some reason even if i specify its path inside angular.json.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./features/auth/components/login/login";
+import { TranslateModule } from '@ngx-translate/core';
+import {
+    TranslateService,
+    TranslatePipe,
+    TranslateDirective
+} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TranslateModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('frontend');
+  private translateService = inject(TranslateService)
+  constructor(){
+    this.translateService.addLangs(["ar", 'en']);
+    this.translateService.setFallbackLang( 'en');
+
+    const browserLang = this.translateService.getBrowserLang()
+    this.translateService.use('en')
+    console.log('brwoser' , browserLang)
+  }
+
 }
