c36bef44acdb60b111215c4091de76f0651728c0
E-HAZMATs <alzamarkal@gmail.com>
2025-12-02 16:26:16 +0300

    Added system user flag to users.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,25 @@
+const { AppError } = require("../utils/responseHandler");
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    roleId: { type: DataTypes.UUID, references: { model: 'Roles', key: "id" }, allowNull: true }
+    roleId: { type: DataTypes.UUID, references: { model: 'Roles', key: "id" }, allowNull: true },
+    isSystemUser: { type: DataTypes.BOOLEAN, defaultValue: false }
+
   }, {
     tableName: 'Users',
     timestamps: true,
-    paranoid: true
+    paranoid: true,
+    hooks: {
+      beforeDestroy: async (user, options) => {
+        if (user.username === 'admin' || user.isSystemUser){
+          throw new AppError("Can't soft delete system users.") // TODO: Localize
+        }
+      },
+    }
   });
 
   User.associate = (models) => {
