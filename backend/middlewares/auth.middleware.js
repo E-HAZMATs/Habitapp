8071302c8bda0146a13339faa8eab2d7ff9f5a8a
E-HAZMATs <alzamarkal@gmail.com>
2025-12-03 10:28:03 +0300

    Updated backend auth middleware .

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,18 +1,19 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { sendError } = require('../utils/responseHandler');
 
 exports.authMiddleware = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if(!authHeader) return res.status(401).send(req.__("accessDenied"));
-    
+    if(!authHeader) return sendError(res, 401, req.__('accessDenied'))
     const token = authHeader.split(' ')[1] // splitting "Bearer" from the token encoded valueand . 
-    if(!token) return res.status(401).send(req.__("accessDenied"))
-
+    if(!token) return sendError(res, 401, req.__("accessDenied"))
+    
     try{
         const decodedToken = jwt.verify(token, process.env.JWT_KEY)
+        console.log('Decoded token:', decodedToken)
         req.user = decodedToken
         next();
     }   
     catch(e){
-        return res.status(401).send(req.__("invalidToken"))
+        return sendError(res, 401, req.__('invalidToken'))
     }
 }
