8c73cca1dc92814cc4806718f35b2d50a03ea944
E-HAZMATs <alzamarkal@gmail.com>
2025-12-08 11:44:34 +0300

    Updated localization service in client.

--- frontend/src/app/core/services/localization-service.ts
+++ frontend/src/app/core/services/localization-service.ts
@@ -1,25 +1,39 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
+export type Language = 'ar' | 'en';
 @Injectable({
   providedIn: 'root',
 })
 export class LocalizationService {
   private translate = inject(TranslateService)
+  private readonly LANG_KEY = 'lang'
+  currentLanguage = signal<Language>('en');
     constructor(){
     this.translate.addLangs(["ar", 'en']);
     this.translate.setFallbackLang( 'en');
     
-    const savedLang = localStorage.getItem('lang');
-    const browserLang = this.translate.getBrowserLang() ?? ''
-    const usedLang: string = savedLang ?? (['en', 'ar'].includes(browserLang) ? browserLang : 'en');
-    localStorage.setItem('lang', usedLang)
-    this.translate.use(usedLang)
-    this.updateHtmlDir(usedLang);
-
+    const savedLang: Language = this.getSavedLang();
+    this.setLanguage(savedLang)
   }
 
-  private updateHtmlDir(lang: string){
+  setLanguage(lang: Language): void {
+    this.currentLanguage.set(lang);
+    this.translate.use(lang);
+    localStorage.setItem(this.LANG_KEY, lang);
+    
     document.documentElement.dir = lang === 'ar' ? 'rtl' : 'ltr';
+    document.documentElement.lang = lang;
+  }
+
+  toggleLanguage(): void {
+    const newLang = this.currentLanguage() === 'en' ? 'ar' : 'en';
+    this.setLanguage(newLang);
+  }
+
+  getSavedLang(): Language {
+    const browserLang = this.translate.getBrowserLang()
+    const defaultLanguage = browserLang ? (['ar', 'en'].includes(browserLang) ? browserLang : 'en') : 'en'
+    return (localStorage.getItem(this.LANG_KEY) as Language) || defaultLanguage
   }
 }
