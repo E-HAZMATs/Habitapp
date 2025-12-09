ce8181f347641b084c4be19bec1a769f88834cdc
E-HAZMATs <alzamarkal@gmail.com>
2025-12-09 21:58:11 +0300

    Added the main pages.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard', //TODOIMP: Implement a guard for dashboard to take u to login if not authed.
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login.component').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'habit-logs',
+    loadComponent: () => import('./features/habit/habit-logs/habit-logs.component').then(m => m.HabitLogsComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '/dashboard'
+  }
+];
---
+++ frontend/src/app/features/habit/habit-logs/habit-logs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-logs.component',
+  imports: [],
+  templateUrl: './habit-logs.component.html',
+  styleUrl: './habit-logs.component.css',
+})
+export class HabitLogsComponent {
+
+}
