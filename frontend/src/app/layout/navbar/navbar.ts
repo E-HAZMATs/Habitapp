274a4646fd3b4c7bbac5b94338894f6483d8db52
E-HAZMATs <alzamarkal@gmail.com>
2025-12-10 16:36:20 +0300

    Checkpoint.

--- frontend/src/app/layout/navbar/navbar.ts
+++ frontend/src/app/layout/navbar/navbar.ts
@@ -1,15 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { MatCard, MatCardContent, MatCardTitle } from "@angular/material/card";
-import { TranslatePipe } from '@ngx-translate/core';
-import { MatAnchor, MatMiniFabButton } from "@angular/material/button";
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { MatAnchor, MatButton, MatButtonModule, MatMiniFabButton } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { ThemeService } from '../../core/services/theme-service';
 import { LocalizationService } from '../../core/services/localization-service';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatCard, MatCardContent, TranslatePipe, MatCardTitle, MatMenuModule, MatMiniFabButton, MatIconModule],
+  imports: [MatCard, MatCardContent, TranslatePipe, MatCardTitle, MatMenuModule, MatMiniFabButton, MatIconModule, TranslateModule, MatButtonModule, MatButton, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
