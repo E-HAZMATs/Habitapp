60846c933f67d9a219ce9be2d123e6721bc9e7ba
E-HAZMATs <alzamarkal@gmail.com>
2025-12-12 22:30:08 +0300

    Navbar touch up. Used toolbar instead of card.

--- frontend/src/app/layout/navbar/navbar.ts
+++ frontend/src/app/layout/navbar/navbar.ts
@@ -1,16 +1,17 @@
-import { Component, inject, signal } from '@angular/core';
-import { MatCard, MatCardContent, MatCardTitle } from "@angular/material/card";
+import { Component, inject } from '@angular/core';
+import { MatCardTitle } from "@angular/material/card";
 import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
-import { MatAnchor, MatButton, MatButtonModule, MatMiniFabButton } from "@angular/material/button";
+import { MatButton, MatButtonModule, MatMiniFabButton } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
 import { ThemeService } from '../../core/services/theme-service';
 import { LocalizationService } from '../../core/services/localization-service';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MatCard, MatCardContent, TranslatePipe, MatCardTitle, MatMenuModule, MatMiniFabButton, MatIconModule, TranslateModule, MatButtonModule, MatButton, RouterLink, RouterLinkActive],
+  imports: [TranslatePipe, MatCardTitle, MatMenuModule, MatMiniFabButton, MatIconModule, TranslateModule, MatButtonModule, MatButton, RouterLink, RouterLinkActive, MatToolbarModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
