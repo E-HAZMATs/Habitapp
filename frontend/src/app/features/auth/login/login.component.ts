a96b741a9af3ebae096e76019b5b7a7227e804b7
E-HAZMATs <alzamarkal@gmail.com>
2025-12-12 23:23:04 +0300

    Login page continued.

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput, MatInputModule } from '@angular/material/input';
+import { TranslatePipe } from '@ngx-translate/core';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-login',
-  imports: [MatCard, MatCardContent, MatCardTitle, MatCardHeader, MatFormField, MatLabel, MatInput],
+  imports: [MatCard, MatCardContent, MatCardTitle, MatCardHeader, MatFormField, MatLabel, MatInput, TranslatePipe, MatIcon],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class Login {
+  protected hidePass: WritableSignal<boolean> = signal(true)
 
+  toggleHidePass(event: MouseEvent){
+    this.hidePass.set(!this.hidePass())
+    console.log(this.hidePass())
+    // event.preventDefault();
+  }
 }
