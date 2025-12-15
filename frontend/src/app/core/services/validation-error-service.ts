f1baa5d87bd4c3219ccfe3e56251e96bc3764f98
E-HAZMATs <alzamarkal@gmail.com>
2025-12-15 23:02:44 +0300

    Added field validation error service.

---
+++ frontend/src/app/core/services/validation-error-service.ts
@@ -0,0 +1,33 @@
+import { inject, Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+
+// TODO: Outsource this to shared?
+type Field = 'email' | 'password' // need better way?
+export const VALIDATION_ERROR_KEYS = {
+  // CHECK: If i have multiple email input fields, do I need to have different names for them?
+  // so thye have a different scope here? or are will all email fields have the same validation?
+  email: {
+    required: "emailRequired",
+    email: "emailInvalid"
+  },
+  password: {
+    required: "passwordRequired"
+  },
+  // TODO: Add fallback/default validation messages?
+} as const
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ValidationErrorService {
+  private translateSerive = inject(TranslateService)
+
+  getValidationError(control: AbstractControl, field: Field): string | null {
+    if (!control || !control.touched || !control.errors) return null;
+    const errorKey = Object.keys(control.errors)[0] as keyof typeof VALIDATION_ERROR_KEYS[Field];
+    const messageKey = VALIDATION_ERROR_KEYS[field][errorKey];
+    const message = messageKey ? this.translateSerive.instant(messageKey) : "THIS IS WRONG COME FIX IT!!" // TODO: Fallback needs default
+    return message;
+  }
+}
