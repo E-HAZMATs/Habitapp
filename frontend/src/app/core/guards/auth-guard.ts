c6e80c4faa160e4152ed48e147d0b9bcdee71c96
E-HAZMATs <alzamarkal@gmail.com>
2025-12-16 23:52:42 +0300

    Handling expired or missing access token (like in case of refreshing page).

--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,16 +1,28 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '../services/token-service';
 import { inject } from '@angular/core';
+import { ApiService } from '../services/api-service';
 
 // TODO: Add logic for when access token is expired?
 export const authGuard: CanActivateFn = (route, state) => {
   const tokenSerive = inject(TokenService)
+  const api = inject(ApiService)
   const router = inject(Router)
 
   const token = tokenSerive.getToken()
   if(token) return true
-  else{
-    router.navigate(['/login'])
-    return false
-  }
+  
+  // Refresh token (in case of refreshing page, token is gone from memory)
+  // Not hitting the endpoint!
+  api.post<{ accessToken: string }>('/auth/refresh', {}).subscribe({
+    next: (res) => {
+      console.log(res)
+    },
+    error: (err:any) => {
+      console.log(err)
+    }
+  })
+  router.navigate(['/login'])
+  return false
+  
 };
