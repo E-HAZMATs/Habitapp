9080e7d285672b1b750f192d279fcb004e13514f
E-HAZMATs <alzamarkal@gmail.com>
2026-01-09 23:00:54 +0300

    Fix backend habit creation validation.

--- backend/validation/habit.validation.js
+++ backend/validation/habit.validation.js
@@ -6,9 +6,9 @@ exports.habitCreateSchema = Joi.object({
   description: Joi.string(),
   frequencyType: Joi.string().valid("daily", "weekly", "monthly").required(),
   frequencyAmount: Joi.number().min(1).required(),
-  dayOfWeek: Joi.number().min(0).max(6),
-  dayOfMonth: Joi.number().min(0).max(30),
-  timeOfDay: Joi.string(), // How to validate Time datatype in Joi?
+  dayOfWeek: Joi.number().min(0).max(6).allow(null),
+  dayOfMonth: Joi.number().min(0).max(30).allow(null),
+  timeOfDay: Joi.string().allow(null), // How to validate Time datatype in Joi?
 }).required();
 
 exports.habitIdSchema = Joi.string().uuid({ version: "uuidv4" }).required();
