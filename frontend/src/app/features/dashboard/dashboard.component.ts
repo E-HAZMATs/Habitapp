5f1a5ca0f0df0d78372d44a36b14bad69d68b475
E-HAZMATs <alzamarkal@gmail.com>
2026-01-12 08:26:54 +0300

    Make create habit modal responsive with app direction.

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CreateHabitModal } from "../habit/create-habit-modal/create-habit-modal";
 import { MatDialog } from '@angular/material/dialog';
+import { LocalizationService } from '../../core/services/localization-service';
 
 @Component({
   selector: 'app-dashboard.component',
@@ -12,9 +13,12 @@ import { MatDialog } from '@angular/material/dialog';
 // my login comp is just Login.
 export class DashboardComponent {
   readonly dialog = inject(MatDialog)
-
+  private localizationService = inject(LocalizationService)
   openDialog() {
-    const dialogRef = this.dialog.open(CreateHabitModal);
+    const currentLang = this.localizationService.currentLanguage();
+    const dialogRef = this.dialog.open(CreateHabitModal, {
+      direction: currentLang === 'ar' ? "rtl" : "ltr"
+    });
     dialogRef.afterClosed().subscribe(result => {
       // console.log('result', result);
     })
