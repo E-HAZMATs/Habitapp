bb4dd1dddda11a5d87fdfb3579699084f2cf58d7
E-HAZMATs <alzamarkal@gmail.com>
2026-01-14 16:28:44 +0300

    checkpoint.

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,19 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { CreateHabitModal } from "../habit/create-habit-modal/create-habit-modal";
 import { MatDialog } from '@angular/material/dialog';
 import { LocalizationService } from '../../core/services/localization-service';
+import { MatButton } from '@angular/material/button';
+import { BidiModule } from "@angular/cdk/bidi";
+import { HabitService } from '../../core/services/habit-service';
+import { habit } from '../../core/models/habit.model';
 
 @Component({
   selector: 'app-dashboard.component',
-  imports: [CreateHabitModal],
+  imports: [CreateHabitModal, MatButton],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 // Agree to a naming convention. when I use ng g and put .component, it adds that to the identifier.
 // my login comp is just Login.
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   readonly dialog = inject(MatDialog)
   private localizationService = inject(LocalizationService)
+  private habitService = inject(HabitService)
+  protected habits = signal<habit[] | null>(null)
+
   openDialog() {
     const currentLang = this.localizationService.currentLanguage();
     const dialogRef = this.dialog.open(CreateHabitModal, {
@@ -22,5 +29,10 @@ export class DashboardComponent {
     dialogRef.afterClosed().subscribe(result => {
       // console.log('result', result);
     })
+  }
+
+  async ngOnInit() {
+    this.habits.set(await this.habitService.getAllByUser());
+    console.log(this.habits())
   }
 }
