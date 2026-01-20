443657e72d25caac3137389838a127eca10262b8
E-HAZMATs <alzamarkal@gmail.com>
2026-01-20 10:53:14 +0300

    dashboard continue.

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { HabitService } from '../../core/services/habit-service';
 import { habit } from '../../core/models/habit.model';
 import { MatCard, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
 import { TranslatePipe } from '@ngx-translate/core';
+import { compareDateDays } from '../../core/utils/compare-date-days';
 
 @Component({
   selector: 'app-dashboard.component',
@@ -39,29 +40,37 @@ export class DashboardComponent implements OnInit {
     let result = await this.habitService.getAllByUser() as any
     let sortedResult: any = []
     result.forEach((habit: any) => {
+      const date = new Date();
+      date.setHours(0, 0, 0, 0);
+      const [hour, minute, second] = habit.timeOfDay ? habit.timeOfDay.split(':').map(Number) : [0, 0, 0];
       if (habit.frequencyType === 'daily') {
-        const date = new Date();
         // Habit never completed before, so due date is today no matter what frequencyAmount (every X *frequencyType*).
         if (!habit.lastCompleted){
-          if(!habit.timeOfDay) {
-            habit.dueIn = date;
-          }
-          else {
-            const [hour, minute, second] = habit.timeOfDay.split(':').map(Number);
-            date.setHours(hour, minute, second)
-            habit.dueIn = date;
-          }
+          date.setHours(hour, minute, second)
+          habit.dueIn = date;
         }
         // TODOIMP: Handle this case - For lastCompleted, a user could have missed completing a habit and thus comparing lastCompleted to freqAmount
         // For example if daily habit is repeated every 2 days (freqAmount = 2), and lastCompleted was 3 days ago, then freqAmount < lastCompleted and not equal.
         // Maybe the user did the habit but didnt log it. So should the habit shown be the one from the day before or the one tommorow? (It's repeated every 2 days and lastCom was 3 days ago.)
         // Maybe implement a worker that sets missed habits as missed? then add a way to log forgotten completions. 
         else {
-          
+          const daysSinceLastComplete = compareDateDays(date, habit.lastCompleted)
+          // CHECK: Should be >= here?
+          if (daysSinceLastComplete === habit.frequencyAmount){
+            const [hour, minute, second] = habit.timeOfDay.split(':').map(Number);
+            date.setHours(hour, minute, second);
+            habit.dueIn = date;
+          }
+          else {
+            const daysTillDue = habit.frequencyAmount - daysSinceLastComplete; // Problem if daysSince > freqAmount!! Need to handle missing completions.
+            if (daysTillDue > 0) // TODO: Delete this? put to prevent duedate in past in case of missing completions.
+              date.setDate(date.getDate() + daysTillDue);
+            
+            habit.dueIn = date;
+          }
         }
       }
       else if (habit.frequencyType === 'weekly') {
-        const date = new Date();
         // debugger;
       }
       else {
