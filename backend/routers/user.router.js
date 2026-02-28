6b94345f392a6eb4935be4a1b732bff9959bc7c3
E-HAZMATs <alzamarkal@gmail.com>
2026-02-28 22:34:11 +0300

    Active page style for log pagination.

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -1,19 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/user.controller')
-const { authMiddleware, hasRole } = require('../middlewares/auth.middleware')
+const { authMiddleware } = require('../middlewares/auth.middleware')
 const { wrapController } = require('../utils/wrapController')
 
 const wrappedController = wrapController(controller);
 
 router.use(authMiddleware)
-router.get('/authRoute', authMiddleware, (req, res) => {
-    res.send('hi')
-})
 router.delete('/delete/:id', wrappedController.delete)
-router.get('/admin', hasRole('admin'), (req, res)=>{
-    res.send('hi admin')
-});
 router.put('/updateTimezone', wrappedController.updateTimezone)
 router.get('/me', wrappedController.me);
 router.patch('/me', wrappedController.updateProfile);
