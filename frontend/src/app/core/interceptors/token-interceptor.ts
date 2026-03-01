8ab463d485044c3cd5fbd9cdc6f792085b478697
E-HAZMATs <alzamarkal@gmail.com>
2026-03-01 22:33:42 +0300

    Prevent multiple concurrent refresh calls when server returns 401.

--- frontend/src/app/core/interceptors/token-interceptor.ts
+++ frontend/src/app/core/interceptors/token-interceptor.ts
@@ -2,8 +2,13 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ApiService } from '../services/api-service';
 import { TokenService } from '../services/token-service';
-import { catchError, switchMap, throwError } from 'rxjs';
+import { catchError, shareReplay, switchMap, throwError } from 'rxjs';
+import { Observable } from 'rxjs';
 import { ENDPOINTS } from '../constants/api-endpoints';
+
+// This observeable is set when first 401 response.
+// Used to prevent multiple refresh calls in case of concurrent 401 reqs.
+let isAlreadyRefreshing$: Observable<any> | null = null;
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   if (req.url.includes('/auth/refresh')) return next(req);
@@ -19,15 +24,24 @@ export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   return next(reqCopy).pipe(
     catchError(err => {
       if (err.status === 401 && !req.url.includes(ENDPOINTS.auth.login)) {
-        return api.get<{ accessToken: string }>(ENDPOINTS.auth.refresh, {}).pipe(
+        // Here, shareReplay(1) caches the response and returns it to all subs. So the req is only executed once.
+        if (!isAlreadyRefreshing$) {
+          isAlreadyRefreshing$ = api.get<{ token: string }>(ENDPOINTS.auth.refresh, {}).pipe(
+            shareReplay(1),
+          );
+        }
+
+        return isAlreadyRefreshing$.pipe(
           switchMap(res => {
-            tokenService.setToken(res.accessToken)
+            isAlreadyRefreshing$ = null;
+            tokenService.setToken(res.token)
             const retryReq = req.clone({
-              headers: req.headers.set('Authorization', `Bearer ${res.accessToken}`)
+              headers: req.headers.set('Authorization', `Bearer ${res.token}`)
             });
             return next(retryReq);
           }),
           catchError(refreshErr => {
+            isAlreadyRefreshing$ = null;
             tokenService.clearToken();
             return throwError(() => refreshErr);
           })
